d79a707fe19e817c409aa3a284a6031adb85d091
Marius OLAR <marius@presslabs.com>
2013-10-14 15:33:10 +0300

    Update class-toplytics-documentation.php

--- class-toplytics-documentation.php
+++ class-toplytics-documentation.php
@@ -29,8 +29,8 @@ class Toplytics_Documentation {
 		<p>You should use this plugin if you want to display the most visited posts of your site, from Google Analytics statistics.</p>
 
 		<h3>2. How can I use the plugin functionality outside the sidebar?</h3>
-		<p>Simple example:</p>
-<code><pre>
+		<p>Simple PHP examples:</p>
+<textarea readonly="readonly" cols="80" rows="8">
 	$toplytics_args = array(
 		'period' => 'month',  // default=month (today/week/month)
 		'numberposts' => 7,   // default=5 (min=1/max=20)
@@ -38,11 +38,11 @@ class Toplytics_Documentation {
 	);
 	if ( function_exists( 'toplytics_results' ) )
 		toplytics_results( $toplytics_args );
-</pre></code>
+</textarea>
 
 		<p>or you can customize your HTML code:</p>
 
-<code><pre>
+<textarea readonly="readonly" cols="80" rows="16">
 	if ( function_exists( 'toplytics_get_results' ) ) {
 		$toplytics_args = array(
 			'period' => 'month',  // default=month (today/week/month)
@@ -58,12 +58,10 @@ class Toplytics_Documentation {
 			}
 		}
 	}
-</pre></code>
+</textarea>
 
 		<h3>3. How to use custom template?</h3>
-		<p>To use your custom template just copy and paste the file `toplytics-template.php` from the toplytics plugin directory to your theme directory.<br /><br />
-The plugin will search first in `toplytics-template.php` file from the theme directory and then will search in the plugin directory, in this case your custom template from the theme structure will be visible first.</p>
-
+		<p>To use your custom template just copy and paste the file `toplytics-template.php` from the toplytics plugin directory to your theme directory, then customize this file.</p><p>The plugin will search first in `toplytics-template.php` file from the theme directory and then will search in the plugin directory, in this case your custom template from the theme structure will be visible first.</p>
 <?php
 	}
 }
