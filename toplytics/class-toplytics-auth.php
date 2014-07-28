5a73c67e9f3ab8977ce2bb2bb0fe1089321a1809
Marius786 <marius@presslabs.com>
2014-07-28 12:42:44 +0300

    Format the code in WP standard

--- toplytics/class-toplytics-auth.php
+++ toplytics/class-toplytics-auth.php
@@ -1,30 +1,25 @@
-<?php
+<?php require_once( dirname( __FILE__ ) . '/OAuth.php' );
 
-require_once( dirname( __FILE__ ) . '/OAuth.php' );
-
-//------------------------------------------------------------------------------
 class Toplytics_Auth {
 	function Toplytics_Auth() {
 		$this->__construct();
 	}
 
-//------------------------------------------------------------------------------
 	function __construct() {
-    	add_action( 'admin_init', array( &$this, 'admin_handle_oauth_login_header' ) );
+		add_action( 'admin_init', array( &$this, 'admin_handle_oauth_login_header' ) );
 	}
 
-//------------------------------------------------------------------------------
 	static function ga_statistics() { // Loading all that's required
 		require_once 'gapi.oauth.class.php'; // GAPI code
 
 		$results = get_transient( 'toplytics.cache' ); // Actual data, cached if possible
 		//if ( $results && time() < ($results['_ts'] + 1800) ) { return $results; }
 
-	  	global $ranges;
-	  	$results = array( '_ts' => time() );
+		global $ranges;
+		$results = array( '_ts' => time() );
 
-	  	try { 
-	  		// The credentials
+		try {
+			// The credentials
 			$account_id   = get_option( 'toplytics_account_id' );
 			$oauth_token  = get_option( 'toplytics_oauth_token' );
 			$oauth_secret = get_option( 'toplytics_oauth_secret' );
@@ -33,7 +28,7 @@ class Toplytics_Auth {
 			$dimensions   = array( 'ga:pagePath' );
 			$metrics      = array( 'ga:pageviews' );
 			$sort         = array( '-ga:pageviews' );
-			$end_date     = date( 'Y-m-d') ;
+			$end_date     = date( 'Y-m-d' );
 			$max_results  = TOPLYTICS_GET_MAX_RESULTS;
 
 			foreach ( $ranges as $name => $start_date ) {
@@ -47,12 +42,11 @@ class Toplytics_Auth {
 				$url .= '&max-results=' . $max_results;
 
 				$ch = curl_init();
-			  
 				// AUTH PROCESS
 				$request_type     = 'GET';
 				$signature_method = new GADOAuthSignatureMethod_HMAC_SHA1();
 
-				$params   = array();
+				$params    = array();
 				$consumer  = new GADOAuthConsumer( 'anonymous', 'anonymous', NULL );
 				$token     = new GADOAuthConsumer( $oauth_token, $oauth_secret );
 				$oauth_req = GADOAuthRequest::from_consumer_and_token( $consumer, $token, $request_type, $url, $params );
@@ -63,7 +57,7 @@ class Toplytics_Auth {
 				// END OF AUTH PROCESS
 
 				if ( defined( TOPLYTICS_DEBUG_MODE ) )
-					error_log( "TOPLYTICS(" . basename( __FILE__ ) . "|" . __LINE__ . ") \$url -> '" . $url . "'\n\n");
+					error_log( 'TOPLYTICS(' . basename( __FILE__ ) . '|' . __LINE__ . ") \$url -> '" . $url . "'\n\n" );
 
 				curl_setopt( $ch, CURLOPT_URL, $url );
 				curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
@@ -82,40 +76,32 @@ class Toplytics_Auth {
 				curl_close( $ch );
 
 				$return_values = array();
-				foreach ( $xml->entry as $entry )
-				{
-					if ( '' == $dimensions )
-					{
+				foreach ( $xml->entry as $entry ) {
+					if ( '' == $dimensions ) {
 						$dim_name = 'value';
-          				}
-          				else
-          				{
-            					$dimension = $entry->xpath( 'dxp:dimension' );
-            					$dimension_attributes = $dimension[0]->attributes();
-            					$dim_name = (string) $dimension_attributes['value'];
-          				}
+					} else {
+						$dimension            = $entry->xpath( 'dxp:dimension' );
+						$dimension_attributes = $dimension[0]->attributes();
+						$dim_name             = (string) $dimension_attributes['value'];
+					}
 
-          				$metric = $entry->xpath( 'dxp:metric' );
-          				if ( 1 < sizeof( $metric ) )
-          				{
-            					foreach ( $metric as $single_metric )
-            					{ 
-              						$metric_attributes = $single_metric->attributes();
-              						$return_values[ $dim_name ][ (string) $metric_attributes['name'] ] = (string) $metric_attributes['value'];
-            					}
-          				}
-          				else
-          				{
-            					$metric_attributes = $metric[0]->attributes();
-            					$return_values[ $dim_name ] = (string) $metric_attributes['value'];
-          				}
+					$metric = $entry->xpath( 'dxp:metric' );
+					if ( 1 < sizeof( $metric ) ) {
+						foreach ( $metric as $single_metric ) {
+							$metric_attributes = $single_metric->attributes();
+							$return_values[ $dim_name ][ (string) $metric_attributes['name'] ] = (string) $metric_attributes['value'];
+						}
+					} else {
+						$metric_attributes = $metric[0]->attributes();
+						$return_values[ $dim_name ] = (string) $metric_attributes['value'];
+					}
 				}
 
 				foreach ( $return_values as $index => $value ) {
-					$link = home_url() . $index;
+					$link    = home_url() . $index;
 					$post_id = url_to_postid( $link );
 
-					if ( "post" == get_post_type ( $post_id ) ) { // filter all posts
+					if ( 'post' == get_post_type( $post_id ) ) { // filter all posts
 						$post = get_post( $post_id );
 						if ( TOPLYTICS_ADD_PAGEVIEWS && $post && isset( $results[ $name ][ $post_id ] ) )
 							$results[ $name ][ $post_id ] += $value;
@@ -127,21 +113,21 @@ class Toplytics_Auth {
 					arsort( $results[ $name ] );
 					$results[ $name ] = array_slice( $results[ $name ], 0, TOPLYTICS_MAX_POSTS, true );
 				}
-
 			} // end foreach ( $ranges as $name...
 		} catch ( Exception $e ) {
-		  	error_log( 'Exception >>> ' . $e );
+			error_log( 'Exception >>> ' . $e );
 			return $results;
 		}
 		set_transient( 'toplytics.cache', $results );
 		if ( defined( TOPLYTICS_DEBUG_MODE ) )
-			error_log( "TOPLYTICS(" . basename( __FILE__ ) . "|" . __LINE__ . ") \$results -> " . print_r( $results, true ) . "\n\n" );
+			error_log( 'TOPLYTICS(' . basename( __FILE__ ) . '|' . __LINE__ . ') $results -> ' . print_r( $results, true ) . "\n\n" );
 
 		return $results;
 	}
 
-//------------------------------------------------------------------------------
-// We have to catch the oauth login data in admin_init so http headers can be added
+	/**
+	 *  We have to catch the oauth login data in admin_init so http headers can be added
+	 */
 	function admin_handle_oauth_login_header() {
 		if ( isset( $_POST['SubmitLogin'] ) && isset( $_POST['toplytics_login_type'] ) && 'oauth' == $_POST['toplytics_login_type'] ) {
 			$this->admin_handle_oauth_login_options();
@@ -150,7 +136,6 @@ class Toplytics_Auth {
 		}
 	}
 
-//------------------------------------------------------------------------------
 	function admin_handle_oauth_login_options() {
 		// Step one in the oauth login sequence is to grab an anonymous token
 		delete_option( 'toplytics_oa_anon_token' );
@@ -175,8 +160,7 @@ class Toplytics_Auth {
 
 		$oa_response = curl_exec( $ch );
 
-		if ( curl_errno( $ch ) )
-		{
+		if ( curl_errno( $ch ) ) {
 			$error_message = curl_error( $ch );
 			$info_redirect = toplytics_get_admin_url( '/options-general.php' ) . '?page=toplytics/toplytics.php&error_message=' . urlencode( $error_message );
 			header( 'Location: ' . $info_redirect );
@@ -185,8 +169,7 @@ class Toplytics_Auth {
 
 		$http_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
 
-		if ( 200 == $http_code)
-		{
+		if ( 200 == $http_code ) {
 			$access_params = $this->split_params( $oa_response );
 
 			add_option( 'toplytics_oa_anon_token', $access_params['oauth_token'] );
@@ -201,9 +184,7 @@ class Toplytics_Auth {
 		die();
 	}
 
-//------------------------------------------------------------------------------
-	function admin_handle_oauth_complete() // step two in oauth login process
-	{
+	function admin_handle_oauth_complete() { // step two in oauth login process
 		if ( function_exists( 'current_user_can' ) && ! current_user_can( 'manage_options' ) )
 			die( __( 'Cheatin&#8217; uh?' ) );
 
@@ -224,13 +205,12 @@ class Toplytics_Auth {
 		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
 
-		$oa_response = curl_exec($ch);
+		$oa_response = curl_exec( $ch );
 
-		if ( curl_errno( $ch ) )
-		{
+		if ( curl_errno( $ch ) ) {
 			$error_message = curl_error( $ch );
-			$info_redirect = toplytics_get_admin_url( '/options-general.php' ) 
-				. '?page=toplytics/toplytics.php&error_message=' 
+			$info_redirect = toplytics_get_admin_url( '/options-general.php' )
+				. '?page=toplytics/toplytics.php&error_message='
 				. urlencode( $error_message );
 
 			header( 'Location: ' . $info_redirect );
@@ -249,25 +229,23 @@ class Toplytics_Auth {
 			update_option( 'toplytics_oauth_secret', $access_params['oauth_token_secret'] );
 			update_option( 'toplytics_auth_token', 'toplytics_see_oauth' );
 
-			$info_redirect = toplytics_get_admin_url( '/options-general.php' ) 
-				. '?page=toplytics/toplytics.php&info_message=' 
+			$info_redirect = toplytics_get_admin_url( '/options-general.php' )
+				. '?page=toplytics/toplytics.php&info_message='
 				. urlencode( 'Authenticated!' );
 
 			header( 'Location: ' . $info_redirect );
 		} else {
-			$info_redirect = toplytics_get_admin_url( '/options-general.php' ) 
-				. '?page=toplytics/toplytics.php&error_message=' 
+			$info_redirect = toplytics_get_admin_url( '/options-general.php' )
+				. '?page=toplytics/toplytics.php&error_message='
 				. urlencode( $oa_response );
 
 			header( 'Location: ' . $info_redirect );
 		}
 
-		die("");
+		die();
 	}
 
-//------------------------------------------------------------------------------
-	function split_params( $response )
-	{
+	function split_params( $response ) {
 		$params = array();
 		$param_pairs = explode( '&', $response );
 		foreach ( $param_pairs as $param_pair ) {
