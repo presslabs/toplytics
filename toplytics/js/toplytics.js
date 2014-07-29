9b21d45a221a6b3b518b316b73f4a4b1095bbb68
Marius786 <marius@presslabs.com>
2014-07-29 19:03:28 +0300

    Fix some JS spelling issues

--- toplytics/js/toplytics.js
+++ toplytics/js/toplytics.js
@@ -1,31 +1,34 @@
 function toplytics_results( args ) {
   var xmlhttp;
-  if ( window.XMLHttpRequest )
+  if ( window.XMLHttpRequest ) {
     xmlhttp = new XMLHttpRequest(); // code for IE7+, Firefox, Chrome, Opera, Safari
-  else
+  } else {
     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); // code for IE6, IE5
+  }
 
   xmlhttp.onreadystatechange = function() {
-    if ( xmlhttp.readyState == 4 && xmlhttp.status == 200 ) {
+    if ( xmlhttp.readyState === 4 && xmlhttp.status === 200 ) {
       var toplytics_json_data = JSON.parse(xmlhttp.responseText);
 
       var results = toplytics_json_data[args.period];
       var k = 0;
       var html = '';
-      for ( index in results ) {
+      for ( var index in results ) {
         var permalink = results[ index ].permalink;
         var title     = results[ index ].title;
         var post_id   = results[ index ].post_id;
         var views     = results[ index ].views;
         k++;
-        if ( k > args.numberposts ) break;
+        if ( k > args.numberposts ) { break };
 
         views_html = "";
-        if ( args.showviews )
+        if ( args.showviews ) {
           views_html = '<span class="post-views">' + views + ' views</span>';
+        }
 
-        if ( permalink && title )
+        if ( permalink && title ) {
           html = html + '<li><a href="' + permalink + '">' + title + '</a>&nbsp;' + views_html + '</li>';
+        }
       }
       var element = document.createElement('ol');
       element.innerHTML = html;
