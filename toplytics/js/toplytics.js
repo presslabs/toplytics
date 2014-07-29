aa2c3f995e04e19a0f85d3e575c07b1c0140d469
Marius786 <marius@presslabs.com>
2014-07-29 19:13:48 +0300

    Fix some JS spelling issues

--- toplytics/js/toplytics.js
+++ toplytics/js/toplytics.js
@@ -14,21 +14,23 @@ function toplytics_results( args ) {
       var k = 0;
       var html = '';
       for ( var index in results ) {
-        var permalink = results[ index ].permalink;
-        var title     = results[ index ].title;
-        //var post_id   = results[ index ].post_id;
-        var views     = results[ index ].views;
-        k++;
-        if ( k > args.numberposts ) { break; }
+          if ( results.hasOwnProperty( index ) ) {
+            var permalink = results[ index ].permalink;
+            var title     = results[ index ].title;
+            //var post_id   = results[ index ].post_id;
+            var views     = results[ index ].views;
+            k++;
+            if ( k > args.numberposts ) { break; }
 
-        var views_html = "";
-        if ( args.showviews ) {
-          views_html = '<span class="post-views">' + views + ' views</span>';
-        }
+            var views_html = "";
+            if ( args.showviews ) {
+                views_html = '<span class="post-views">' + views + ' views</span>';
+            }
 
-        if ( permalink && title ) {
-          html = html + '<li><a href="' + permalink + '">' + title + '</a>&nbsp;' + views_html + '</li>';
-        }
+            if ( permalink && title ) {
+                html = html + '<li><a href="' + permalink + '">' + title + '</a>&nbsp;' + views_html + '</li>';
+            }
+          }
       }
       var element = document.createElement('ol');
       element.innerHTML = html;
