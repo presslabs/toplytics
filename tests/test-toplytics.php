02207f7f7620f422833d37d7aae67d129dd1b1e6
Marius786 <marius@presslabs.com>
2015-04-14 18:53:53 +0300

    update the tests

--- tests/test-toplytics.php
+++ tests/test-toplytics.php
@@ -9,16 +9,10 @@ class Test_Toplytics extends Toplytics_UnitTestCase {
 		global $toplytics;
 		$this->toplytics = $toplytics;
 
-		set_transient(
-			'toplytics_cached_results',
-			array(
-				'_ts'     => time(),
-				'monthly' => array( '166384' => '864', '166369' => '84' ),
-				'2weeks'  => array( '166384' => '400', '166369' => '40' ),
-				'weekly'  => array( '166384' => '300', '166369' => '30' ),
-				'daily'   => array( '166384' => '100', '166369' => '10' ),
-			)
-		);
+		add_option( 'toplytics_result_today', array( 'result' => array( '166384' => '100', '166369' => '10' ) ) );
+		add_option( 'toplytics_result_month', array( 'result' => array( '166384' => '864', '166369' => '84' ) ) );
+		add_option( 'toplytics_result_week', array( 'result' => array( '166384' => '300', '166369' => '30' ) ) );
+		add_option( 'toplytics_result_2weeks', array( 'result' => array( '166384' => '400', '166369' => '40' ) ) );
 	}
 
 	function teardown() {
@@ -29,14 +23,14 @@ class Test_Toplytics extends Toplytics_UnitTestCase {
 	}
 
 	function test_toplytics_get_result_case_1() {
-		$assert = $this->toplytics->get_result( 'daily' ) == array(
+		$assert = $this->toplytics->get_result( 'today' ) == array(
 			'166384' => '100', '166369' => '10',
 		);
 		$this->assertTrue( $assert );
 	}
 
 	function test_toplytics_get_result_case_2() {
-		$assert = $this->toplytics->get_result( 'weekly' ) == array(
+		$assert = $this->toplytics->get_result( 'week' ) == array(
 			'166384' => '300', '166369' => '30',
 		);
 		$this->assertTrue( $assert );
@@ -50,7 +44,7 @@ class Test_Toplytics extends Toplytics_UnitTestCase {
 	}
 
 	function test_toplytics_get_result_case_4() {
-		$assert = $this->toplytics->get_result( 'monthly' ) == array(
+		$assert = $this->toplytics->get_result( 'month' ) == array(
 			'166384' => '864', '166369' => '84',
 		);
 		$this->assertTrue( $assert );
