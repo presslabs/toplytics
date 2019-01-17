895d4112b2a7edd6fc0f0678c1bd9b6c669a73e4
Cristi @ Presslabs <cristi@presslabs.com>
2019-01-17 20:08:55 +0200

    Update tests bootstrap and add basic test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,19 @@
 <?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package My_Plugin
+ */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
+	exit( 1 );
 }
 
 // Give access to tests_add_filter() function.
@@ -14,7 +25,6 @@ require_once $_tests_dir . '/includes/functions.php';
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/src/toplytics.php';
 }
-
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
