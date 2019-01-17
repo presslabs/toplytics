507069bb128652d570a760273002651e00cbb45b
Cristi @ Presslabs <cristi@presslabs.com>
2019-01-17 20:08:55 +0200

    Update tests bootstrap and add basic test

---
+++ tests/basic-test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class BasicTest
+ *
+ * @package Toplytics
+ */
+
+/**
+ * Basic test case.
+ */
+class BasicTest extends WP_UnitTestCase {
+
+	/**
+	 * A single basic test.
+	 */
+	public function test_basic() {
+		$this->assertTrue( true );
+	}
+}
