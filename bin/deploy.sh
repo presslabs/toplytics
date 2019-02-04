4328665da4e95c5e03a5bc21e425d1a43250d7b5
Cristi @ Presslabs <cristi@presslabs.com>
2019-02-04 16:49:38 +0200

    make deploy script more vorbose and fix cp

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,6 +5,9 @@ set -x
 
 # This is used by Travis CI to deploy automatically to SVN when a new tag is created!
 
+# Get a sense of where we are for debuging
+pwd
+
 # Clone complete SVN repository to separate directory
 svn co $SVN_REPOSITORY ../svn
 
@@ -13,16 +16,30 @@ rm -rf ../svn/trunk/
 rm -rf ../svn/assets/
 rm -rf ../svn/tags/$TRAVIS_TAG/
 
-# Copy plugin src contents to SNV trunk/ directory
-cp -R ./src/ ../svn/trunk/
-cp -R ./assets/ ../svn/assets/
+# Recreate directories after clean-up
+mkdir -p ../svn/trunk/
+mkdir -p ../svn/assets/
+mkdir -p ../svn/tags/$TRAVIS_TAG/
 
-# Go to SVN repository root
+echo "Listing svn dir after clean-up."
+ls -la ../svn/
+
+# Copy plugin src contents to SNV trunk/ directory
+cp -r ./src/ ../svn/trunk/
+cp -r ./assets/ ../svn/assets/
+
+# Go to SVN repository root and list
 cd ../svn/
 
+echo "Listing svn dir after copy of trunk and assets."
+ls -la
+
 # Create SVN tag
-mkdir ./tags/$TRAVIS_TAG
-svn cp trunk ./tags/$TRAVIS_TAG
+cp -r trunk/ tags/$TRAVIS_TAG/
+
+# Add svn tag
+svn add tags/$TRAVIS_TAG/
+svn status
 
 # Push SVN tag
 svn ci  --message "Release $TRAVIS_TAG" \
