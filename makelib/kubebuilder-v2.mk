a3095bc594cc252eb5ced6fddc3780e9bf8ff2a5
Robert Stefan <rstefan1@users.noreply.github.com>
2021-07-08 11:59:54 +0300

    Use `go install` instead of `go get` to install go tools

--- makelib/kubebuilder-v2.mk
+++ makelib/kubebuilder-v2.mk
@@ -51,7 +51,7 @@ $(KUBEBUILDER):
 	@rm -fr $(TOOLS_HOST_DIR)/tmp
 	@$(OK) installing kubebuilder $(KUBEBUILDER_VERSION)
 
-$(eval $(call tool.go.get,controller-gen,v0.2.4,sigs.k8s.io/controller-tools/cmd/controller-gen))
+$(eval $(call tool.go.install,controller-gen,v0.2.4,sigs.k8s.io/controller-tools/cmd/controller-gen))
 
 # ====================================================================================
 # Kubebuilder Targets
