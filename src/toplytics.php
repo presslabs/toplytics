93a4883663d6e31b7a6a7438e502fc1c50385518
denis <denis@presslabs.com>
2026-08-17 19:25:10 +0300

    fix warning md5 caused by recent changes in the template

--- src/toplytics.php
+++ src/toplytics.php
@@ -45,7 +45,7 @@ define('TOPLYTICS_CUSTOM_TEMPLATE_DEFAULT_NAME', 'toplytics-template');
 define('TOPLYTICS_AUTH_API_VERSION', '1');
 define('TOPLYTICS_AUTH_API_BASE_URL', 'https://toplytics.presslabs.org/toplytics/');
 define('TOPLYTICS_AUTH_API_BASE_CONFIG', 'apiconfig.json');
-define('TOPLYTICS_WIDGET_TEMPLATE_VERSION', '2039481e7f61d8ee7f3cdd3dea2b0689');
+define('TOPLYTICS_WIDGET_TEMPLATE_VERSION', '6566a396e2440d22800efc13c6d93720');
 define('TOPLYTICS_FOLDER_ROOT', plugin_dir_path(__FILE__));
 define('TOPLYTICS_MAX_API_ERRORS_COUNT', 20);
 
